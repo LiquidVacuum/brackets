0c3ffb056f61130e39d5cd9b8c112c3f73570018
LiquidVacuum <venomandroid@gmail.com>
2021-03-09 01:54:37 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketPairsCollection = [];
+  for (const pair of bracketsConfig) {
+    bracketPairsCollection.push(pair.join(''));
+  }
+  
+  let string = str;
+  let flag = true;
+  
+  while (flag) {
+    if (string.length == 0) return true; 
+    
+    flag = false;
+    
+    for (const pair of bracketPairsCollection) { 
+      let pos = string.indexOf(pair);
+      if (pos < 0) continue;
+      flag = true;
+      string = string.slice(0, pos) + string.slice(pos + 2);
+    }
+  }
+  return false;
 }
